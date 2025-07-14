03a0a4505987feb283031da7950ac6e7a3960564
SightFad <136160744+SightFad@users.noreply.github.com>
2025-07-14 13:27:09 +0700

    #3

--- LibraryBackEnd/LibraryApi/Program.cs
+++ LibraryBackEnd/LibraryApi/Program.cs
@@ -3,19 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
-        policy => policy.WithOrigins("https://quan-ly-thu-vien-web.vercel.app/")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod());
+        policy => policy
+            .WithOrigins("https://quan-ly-thu-vien-web.vercel.app")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
 });
 
 var app = builder.Build();
@@ -25,11 +26,9 @@ app.UseCors("AllowReactApp");
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<LibraryContext>();
-    SeedData.Initialize(context);
+    context.Database.Migrate(); 
+    SeedData.Initialize(context); 
 }
-
-app.UseCors(policy =>
-    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 if (app.Environment.IsDevelopment())
 {
